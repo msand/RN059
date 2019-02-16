720be5fa4538da8c695a989044ba6a4d142421c0
Mikael Sand <msand@abo.fi>
2019-02-16 23:10:18 +0200

    https://github.com/react-native-community/react-native-svg/issues/931

--- App.js
+++ App.js
@@ -1,29 +1,36 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component, useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Svg, Path } from 'react-native-svg';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+function SvgTest() {
+  return (
+    <Svg height="100%" width="100%" viewBox="0 0 100 100">
+      <Path d="M0,0 L100,100" stroke="black" />
+    </Svg>
+  );
+}
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const Home = () => {
+  const [display, setDisplay] = useState('flex');
+  return (
+    <View>
+      <TouchableOpacity
+        onPress={() => setDisplay(display === 'flex' ? 'none' : 'flex')}
+      >
+        <Text style={styles.text}> Testing display attribute </Text>
+      </TouchableOpacity>
+      <View style={[styles.svg, { display }]}>
+        <SvgTest />
+      </View>
+    </View>
+  );
+};
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Home />
       </View>
     );
   }
@@ -32,18 +39,14 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    padding: 50,
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
+  text: {
     color: '#333333',
-    marginBottom: 5,
+  },
+  svg: {
+    alignItems: 'center',
   },
 });
